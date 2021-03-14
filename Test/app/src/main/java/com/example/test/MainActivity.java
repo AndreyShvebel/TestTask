1fd77c3ad4a4632c97daa6e9cd8658294d09e903
Andrey Shvebel <andrei_shvebel@mail.ru>
2021-03-15 02:53:09 +0600

    commit

---
+++ Test/app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.test;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+    Button bChangeColor, bResetColor;
+    protected float r;
+    protected float g;
+    protected float b;
+    LinearLayout layout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bChangeColor =findViewById(R.id.b1);
+        bResetColor =findViewById(R.id.b2);
+        layout=findViewById(R.id.ll);
+
+        bChangeColor.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View v) { //generates and sets new colors on bg when button is clicked
+                r = (float) Math.random();
+                g = (float) Math.random();
+                b = (float) Math.random();
+                layout.setBackgroundColor(Color.rgb(r,g,b));
+            }
+        });
+
+        bResetColor.setOnClickListener(new View.OnClickListener(){ //resets bg color to default (white)
+            @Override
+            public void onClick(View v) {
+                layout.setBackgroundColor(Color.WHITE);
+            }
+        });
+    }
+}
